d1787c6a0b7a1e7fccfcfb7d6f633386a31ad55b
Christopher Lindblom <lindblom@doless.se>
2012-03-14 18:55:33 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ManUp::Application.config.secret_token = '968b1d3c3cf22b10bd95686405e7407d3f9ce3e653013ed9f7c23bc75956f596f502e9e2df39cb75ba38bd8010e2a43cc89b3af2689d4d519b2cf2e8695044ef'
