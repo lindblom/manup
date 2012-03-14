f1f4ce3dbc9a50d7623f96d18fb1d6fe14971845
Christopher Lindblom <lindblom@doless.se>
2012-03-14 19:02:41 +0100

    added helper methods for title and description, added description to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  def set_title(title)
+    content_for :title do
+      title
+    end
+  end
+  
+  def set_description(description)
+    content_for :description do
+      description
+    end
+  end
 end
